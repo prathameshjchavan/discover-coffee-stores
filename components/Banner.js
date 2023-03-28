5c8f5c1e9e36420151ef98154a8c70f99f74e61b
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-03-28 16:34:07 +0530

    building banner

---
+++ components/Banner.js
@@ -0,0 +1,20 @@
+import styles from "./Banner.module.css";
+
+import React from "react";
+
+function Banner({ buttonText, handleOnClick }) {
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>
+				<span className={styles.title1}>Coffee</span>{" "}
+				<span className={styles.title2}>Connoisseur</span>
+			</h1>
+			<p className={styles.subtitle}>Discover your local coffee shops</p>
+			<button className={styles.button} onClick={handleOnClick}>
+				{buttonText}
+			</button>
+		</div>
+	);
+}
+
+export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+	const handleOnBannerBtnClick = () => {
+		console.log("Hi banner button");
+	};
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -11,7 +15,10 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>Coffee Connoisseur</h1>
+				<Banner
+					buttonText="View stores nearby"
+					handleOnClick={handleOnBannerBtnClick}
+				/>
 			</main>
 		</div>
 	);
