e07dc7b3cf5bbc2c1a91035b06b6a06b0c221512
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-03-28 19:53:51 +0530

    styling home page

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,5 @@
 import styles from "./Banner.module.css";
 
-import React from "react";
-
 function Banner({ buttonText, handleOnClick }) {
 	return (
 		<div className={styles.container}>
@@ -9,10 +7,12 @@ function Banner({ buttonText, handleOnClick }) {
 				<span className={styles.title1}>Coffee</span>{" "}
 				<span className={styles.title2}>Connoisseur</span>
 			</h1>
-			<p className={styles.subTitle}>Discover your local coffee shops</p>
-			<button className={styles.button} onClick={handleOnClick}>
-				{buttonText}
-			</button>
+			<p className={styles.subTitle}>Discover your local coffee shops!</p>
+			<div className={styles.buttonWrapper}>
+				<button className={styles.button} onClick={handleOnClick}>
+					{buttonText}
+				</button>
+			</div>
 		</div>
 	);
 }
--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,16 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const CoffeeStore = () => {
 	const router = useRouter();
 	const { id } = router.query;
 
-	return <div>Coffee Store Page {id}</div>;
+	return (
+		<div>
+			<p>Coffee Store Page {id}</p>
+			<Link href="/">Back to home</Link>
+		</div>
+	);
 };
 
 export default CoffeeStore;
