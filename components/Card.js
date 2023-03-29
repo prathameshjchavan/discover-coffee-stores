be1ec34166f1c0cadbb19f9d0b3b6f88bb09c31f
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-03-29 17:50:54 +0530

    styling card component

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./Card.module.css";
+import cls from "classnames";
 
 const Card = ({ name, imgUrl, href }) => {
 	return (
-		<Link href={href}>
-			<h2>{name}</h2>
-			<Image src={imgUrl} width={260} height={160} alt="name" />
+		<Link href={href} className={styles.cardLink}>
+			<div className={cls("glass", styles.container)}>
+				<div className={styles.cardHeaderWrapper}>
+					<h2 className={styles.cardHeader}>{name}</h2>
+				</div>
+				<div className={styles.cardImageWrapper}>
+					<Image
+						className={styles.cardImage}
+						src={imgUrl}
+						width={260}
+						height={160}
+						alt="name"
+					/>
+				</div>
+			</div>
 		</Link>
 	);
 };
