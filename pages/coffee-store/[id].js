b1333d624b7f8acbd5ae25620b3ac5521f021ce5
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-03-30 17:18:06 +0530

    store pages ssg

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,7 +1,29 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import coffeeStoresData from "../../data/coffee-stores.json";
 
-const CoffeeStore = () => {
+export function getStaticProps(context) {
+	return {
+		props: {
+			coffeeStore: coffeeStoresData.find(
+				(coffeeStore) => coffeeStore.id.toString() === context.params.id
+			),
+		},
+	};
+}
+
+export function getStaticPaths() {
+	return {
+		paths: [
+			{ params: { id: "0" } },
+			{ params: { id: "1" } },
+			{ params: { id: "300" } },
+		],
+		fallback: false,
+	};
+}
+
+const CoffeeStore = ({ coffeeStore }) => {
 	const router = useRouter();
 	const { id } = router.query;
 
@@ -9,6 +31,8 @@ const CoffeeStore = () => {
 		<div>
 			<p>Coffee Store Page {id}</p>
 			<Link href="/">Back to home</Link>
+			<p>{coffeeStore.address}</p>
+			<p>{coffeeStore.name}</p>
 		</div>
 	);
 };
