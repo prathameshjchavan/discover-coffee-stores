f7fe0efb67de94b34ff74a6984315ba940b87953
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-03-30 11:46:45 +0530

    homepage ssg

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,13 @@ import Card from "../components/Card";
 import styles from "../styles/Home.module.css";
 import coffeeStores from "../data/coffee-stores.json";
 
-const Home = () => {
+export async function getStaticProps(context) {
+	return {
+		props: { coffeeStores },
+	};
+}
+
+const Home = (props) => {
 	const handleOnBannerBtnClick = () => {
 		console.log("Hi banner button");
 	};
@@ -29,10 +35,11 @@ const Home = () => {
 						width={700}
 						height={400}
 						alt="hero image"
+						priority
 					/>
 				</div>
 				<div className={styles.cardLayout}>
-					{coffeeStores.map((store) => (
+					{props.coffeeStores.map((store) => (
 						<Card
 							key={store.id}
 							name={store.name}
