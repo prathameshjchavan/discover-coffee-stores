ed22891b9e4ca3d973a40cf9de54518505d986d7
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-03-31 08:49:04 +0530

    fallback paths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -14,18 +14,18 @@ export function getStaticProps(context) {
 
 export function getStaticPaths() {
 	return {
-		paths: [
-			{ params: { id: "0" } },
-			{ params: { id: "1" } },
-			{ params: { id: "300" } },
-		],
-		fallback: false,
+		paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+		fallback: true,
 	};
 }
 
 const CoffeeStore = ({ coffeeStore }) => {
 	const router = useRouter();
 	const { id } = router.query;
+
+	if (router.isFallback) {
+		return <div>Loading...</div>;
+	}
 
 	return (
 		<div>
