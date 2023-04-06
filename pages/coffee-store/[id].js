1254373603e66df897d4ac0d5f99a5cfbd11c7a2
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-04-06 08:54:55 +0530

    white background fix

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -48,7 +48,7 @@ const CoffeeStore = ({ coffeeStore }) => {
 			<div className={styles.container}>
 				<div className={styles.col1}>
 					<div className={styles.backToHomeLink}>
-						<Link href="/">Back to home</Link>
+						<Link href="/">← Back to home</Link>
 					</div>
 					<div className={styles.nameWrapper}>
 						<h1 className={styles.name}>{name}</h1>
