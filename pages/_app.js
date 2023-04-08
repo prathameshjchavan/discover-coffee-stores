3240340c51c1a3020e161452a8f39d1d0dd7895c
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-04-08 18:23:15 +0530

    creating store reducer

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "../styles/globals.css";
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const IBMPlexSans = localFont({
 	src: [
@@ -15,11 +15,28 @@ const initialState = {
 	latLong: "",
 	coffeeStores: [],
 };
+const ACTION_TYPES = {
+	SET_LAT_LONG: "SET_LAT_LONG",
+	SET_COFFEE_STORES: "SET_COFFEE_STORES",
+};
+
+const storeReducer = (state, action) => {
+	switch (action.type) {
+		case ACTION_TYPES.SET_LAT_LONG:
+			return { ...state, latLong: action.payload.latLong };
+		case ACTION_TYPES.SET_COFFEE_STORES:
+			return { ...state, coffeeStores: action.payload.coffeeStores };
+		default:
+			throw new Error(`Unhandled action type: ${action.type}`);
+	}
+};
 
 function MyApp({ Component, pageProps }) {
+	const [state, dispatch] = useReducer(storeReducer, initialState);
+
 	return (
 		<div className={IBMPlexSans.className}>
-			<StoreContext.Provider value={initialState}>
+			<StoreContext.Provider value={{ state, dispatch }}>
 				<Component {...pageProps} />
 			</StoreContext.Provider>
 		</div>
