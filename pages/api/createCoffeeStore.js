fb355b7cc7e9b4fe1e0fd67e057649f90c9ee586
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-04-23 18:01:55 +0530

    configuring airtable

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,6 @@
+import Airtable from "airtable";
+
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+	process.env.AIRTABLE_BASE_KEY
+);
+const table = base("coffee-stores");
