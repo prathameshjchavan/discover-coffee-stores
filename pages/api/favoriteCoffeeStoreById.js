ce464b01ec2ce8b2f7c93380f597651270c1db96
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-04-30 20:38:22 +0530

    creating favoriteCoffeeStoreById

---
+++ pages/api/favoriteCoffeeStoreById.js
@@ -0,0 +1,48 @@
+import {
+	findRecordByFilter,
+	getMinifiedRecords,
+	table,
+} from "../../lib/airtable";
+
+const favoriteCoffeeStoreById = async (req, res) => {
+	if (req.method === "PUT") {
+		try {
+			const { id } = req.body;
+
+			if (id) {
+				// find a record
+				const records = await findRecordByFilter(id);
+
+				if (records.length !== 0) {
+					const record = records[0];
+					const calculateVoting = parseInt(record.voting) + 1;
+
+					// update a record
+					const updatedRecords = await table.update([
+						{
+							id: record.recordId,
+							fields: {
+								voting: calculateVoting,
+							},
+						},
+					]);
+
+					if (updatedRecords) {
+						const minifiedRecords = getMinifiedRecords(updatedRecords);
+						res.json(minifiedRecords);
+					}
+				} else {
+					res.json({ message: "Coffee store id doesn't exist", id });
+				}
+			} else {
+				res.status(400);
+				res.json({ message: "Id is missing" });
+			}
+		} catch (error) {
+			res.status(500);
+			res.json({ message: "Error upvoting coffee store", error });
+		}
+	}
+};
+
+export default favoriteCoffeeStoreById;
